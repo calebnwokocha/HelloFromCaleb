560f58dd11ba94da3036bb1605f4a5d140e96d33
Caleb Nwokocha <calebnwokocha@gmail.com>
2021-05-17 17:50:13 -0500

    Initial commit

---
+++ src/HelloFromCaleb.java
@@ -0,0 +1,15 @@
+
+public class HelloFromCaleb {
+    public HelloFromCaleb(){
+
+    }
+
+    public void greeting(){
+        System.out.println("Hello my name is Caleb.");
+    }
+
+    public static void main(String[] args){
+        HelloFromCaleb h = new HelloFromCaleb();
+        h.greeting();
+    }
+}
